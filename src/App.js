b73c9aafbd43b3d91407e06997503477784db5a9
Pranay Kumar Andra <pka.pranayandra02@gmail.com>
2021-03-21 10:37:29 +0530

    auth wip

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import Userfront from "@userfront/react";
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import Login from './auth/Login';
+
+Userfront.init(process.env.REACT_APP_id);
 
 function App() {
-  return (
+    return (
+    <Router>
     <div className="App">
-      <h2>testing</h2>
+          <nav>
+              {/* <button>
+                  <Link to="/sign-up">Sign up</Link>
+              </button> */}
+              <button>
+                  <Link to="/login">Login</Link>
+              </button>
+          </nav>
+            <Switch>
+              <Route path="/login">
+                  <Login />
+              </Route>
+          </Switch>
     </div>
+    </Router>
   );
 }
 
