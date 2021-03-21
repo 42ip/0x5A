3a5dcdde16938481f4f23991146bfff9ca280849
Pranay Kumar Andra <pka.pranayandra02@gmail.com>
2021-03-21 16:23:14 +0530

    Basic Profile content

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,13 @@
 import {Component} from 'react'
-
+import Avatar from 'react-avatar';
+import './Profile.css'
 export default class Profile extends Component {
     render()
     {
         return (
-            <div>
-            <h2>hey from profile page</h2>
+            <div className="match-page">
+                <Avatar name="Jennifer" size="150" textSizeRatio={1.75} />
+                <Avatar name="Ben" size="150" textSizeRatio={1.75} />
             </div>
         );
     }
