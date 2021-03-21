76dc341d970ea0931340b8786cba4f8e9774c95c
Raghavendra Achar <raghav142001@gmail.com>
2021-03-21 16:12:55 +0530

    logout

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,19 @@ import Reset from "../auth/Reset";
 import "../App.css";
 import Userfront from "@userfront/react";
 export default class Home extends Component {
+	
   render() {
+	function settingfun(e){
+		if(document.querySelector(".settings").style.display==="block"){
+			document.querySelector(".settings").style.display="none";
+		}else{
+		document.querySelector(".settings").style.display="block";
+		}
+	}
+
+	function settingfun2(e){
+			document.querySelector(".settings").style.display="none";
+	}
     return (
       <Router>
         <div className="App">
@@ -13,19 +25,19 @@ export default class Home extends Component {
               <Link to="/">APPNAME</Link>
             </button>
             <button class="nav-button" onClick={Userfront.logout}>
-              <Link to="/">Logout</Link>
+              <Link >Logout</Link>
             </button>
 			<button class="nav-button">
-              <Link to="/">Matches</Link>
+              <Link >Matches</Link>
             </button>
-            <button class="nav-button">
-              <Link to="/">Settings</Link>
+            <button class="nav-button" onClick={settingfun}>
+              <Link >Settings</Link>
             </button>
           </nav>
 		  <div class="settings">
 			  <h4 id="1">Profile</h4>
 			  <h4 id="2">Chat</h4>
-			  <h4 id="3"><Link to="/reset" style={{textDecoration: "none" }}>Change Password</Link></h4>
+			  <h4 id="3"><Link to="/reset" style={{textDecoration: "none" }} onClick={settingfun2}>Change Password</Link></h4>
 		  </div>
           <Switch>
             <Route path="/dashboard"></Route>
