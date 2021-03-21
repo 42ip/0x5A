f6796f03bdfe2a4ae69ca5308c9c587de0e4e4f8
Raghavendra Achar <raghav142001@gmail.com>
2021-03-21 11:07:34 +0530

    home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { Switch, BrowserRouter as Router, Link , Route} from "react-router-dom";
 import './App.css';
-
+import Home from './components/Home.js';
 function App() {
   return (
+    <Router>
     <div className="App">
-      <h2>testing</h2>
+    <nav>
+              <button class="nav-button appname">
+                  <Link to="/">APPNAME</Link>
+              </button>
+              <button class="nav-button">
+                <Link to="/">Sign Out</Link>
+              </button> 
+              <button class="nav-button">
+                <Link to="/">Settings</Link>
+              </button>
+     </nav>
+              <Switch>
+              <Route path="/">
+                    <Home />
+                </Route>
+              </Switch>
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+import '../App.css';
+
+export default class Home extends Component {
+  render()
+    {
+	return(
+		<div><h1 class="home-h1">Explore </h1></div>
+	);  
+}
+}
